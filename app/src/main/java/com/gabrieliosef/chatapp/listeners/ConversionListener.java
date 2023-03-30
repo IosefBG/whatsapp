b95be2873d650e78ffe975e446dbac927a4aa7c1
IosefBG <iosefgabriel268@gmail.com>
2023-03-30 23:27:33 +0300

    init

---
+++ app/src/main/java/com/gabrieliosef/chatapp/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package com.gabrieliosef.chatapp.listeners;
+
+import com.gabrieliosef.chatapp.models.User;
+
+public interface ConversionListener {
+    void onConversionClick(User user);
+}
---
+++ app/src/main/java/com/gabrieliosef/chatapp/listeners/UserListener.java
@@ -0,0 +1,7 @@
+package com.gabrieliosef.chatapp.listeners;
+
+import com.gabrieliosef.chatapp.models.User;
+
+public interface UserListener {
+    void onUserClicked(User user);
+}
